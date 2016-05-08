3adedb1c7d8f2dc86968f8a7ec1fe2fd17909930
phil <uniphil@gmail.com>
2016-05-08 15:06:55 -0400

    basically work

--- photos.js
+++ photos.js
@@ -1,5 +1,8 @@
 ((c, Immutable, Union, Async, Effect, fb, d, t) => {
 
+  const SHOWTIME = 4000;
+  const TRANSTIME = 1000;
+
   const Actions = Union({
     LoadPhotos: null,
     StartAutoplay: null,
@@ -10,8 +13,19 @@
     loadState: Async.Pending(),
     photos: Immutable.List(),
     selected: 0,
+    trans: 0,
     t0: 0,
   });
+
+  const getNext = state => {
+    const current = state.get('selected');
+    const total = state.get('photos').size;
+    if (total > 0) {
+      return (current + 1) % total;
+    } else {
+      return current;
+    }
+  };
 
   const init = () => ({
     state: State(),
@@ -27,7 +41,10 @@
   const update = (state, action) => Actions.match(action, {
     LoadPhotos: result => Async.match(result, {
       _: () => ({
-        effects: [],
+        effects: [{
+          effect: Effect.Tick(),
+          wrap: Actions.StartAutoplay,
+        }],
         state: state.set('loadState', result),
       }),
       Done: data => ({
@@ -46,25 +63,40 @@
       }],
       state: state.set('t0', t0),
     }),
-    Tick: t => ({
-      effects: [{
-        effect: Effect.Tick(),
-        wrap: Actions.Tick,
-      }],
-      state: (t - state.get('t0')) < 2000
-        ? state
-        : state
-          .set('t0', t)
-          .update('selected', n => {
-            const total = state.get('photos').size;
-            if (total > 0) {
-              return (n + 1) % total;
-            } else {
-              return n;
-            }
-          }),
-    }),
+    Tick: t => {
+      const dt = t - state.get('t0');
+      let nextState = state;
+      if (SHOWTIME <= dt && dt < (SHOWTIME + TRANSTIME)) {
+        nextState = state
+          .set('trans', (dt - SHOWTIME) / TRANSTIME);
+      } else if (dt >= (SHOWTIME + TRANSTIME)) {
+        nextState = state.merge({
+          selected: getNext(state),
+          trans: 0,
+          t0: t,
+        });
+      }
+      return {
+        effects: [{
+          effect: Effect.Tick(),
+          wrap: Actions.Tick,
+        }],
+        state: nextState,
+      };
+    },
   });
+
+
+  const imStyle = {
+    backgroundPosition: '50%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    height: '100%',
+    left: '0',
+    position: 'absolute',
+    top: '0',
+    width: '100%',
+  };
 
 
   const View = (state, dispatch) =>
@@ -73,15 +105,22 @@
       Errored: () => d('p', {}, [t('Error loading photos >:')]),
       Done: () => d('div', { attrs: { style: {
         backgroundColor: '#000',
-        backgroundImage: `url(${state.getIn(['photos', state.get('selected')])})`,
-        backgroundPosition: '50%',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
         height: '100%',
+        position: 'relative',
         width: '100%',
       } }, events: {
         click: () => dispatch(Actions.StartAutoplay()),
-      } }, []),
+      } }, [
+        d('div', { attrs: { style: o.merge(imStyle, {  // preloader
+          backgroundImage: `url(${state.getIn(['photos', getNext(state)])})`,
+          opacity: `${Math.max(0, state.get('trans') - (1 / 3)) * (3 / 2)}`,
+          zIndex: `${state.get('trans') > 0 ? '1' : '0'}`,
+        }) } }, []),
+        d('div', { attrs: { style: o.merge(imStyle, {
+          backgroundImage: `url(${state.getIn(['photos', state.get('selected')])})`,
+          opacity: `${1 - state.get('trans')}`,
+        }) } }, []),
+      ]),
     });
 
   Object.assign(c, {
